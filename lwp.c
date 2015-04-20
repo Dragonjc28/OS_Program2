ebb85e83e5deda0de56b8a35bd2e024483628252
JustinRoll <jroll@csc235x12.ad.calpoly.edu>
2015-04-19 18:25:41 -0700

    Program now compiles

--- lwp.c
+++ lwp.c
@@ -7,20 +7,24 @@
 #include <stdlib.h>
 #include "lwp.h"
 
-#define prev lib_one
-#define next lib_two
+#define tprev lib_one
+#define tnext lib_two
 #define sprev sched_one
 #define snext sched_two 
 
+context* rr_next();
+void rr_remove(thread victim);
+void rr_admit(thread new);
 
 /* GLOBALS */
-static scheduler sched = {NULL, NULL, rr_admit, rr_remove, rr_next};
+static struct scheduler sched_global = {NULL, NULL, rr_admit, rr_remove, rr_next};
+static scheduler sched = &sched_global;
 static rfile returnContext; /* pointer to original return context */
-static context runningThread = NULL; /* global for the currently running thread */
-static context head;
+static void* returnSP; /* pointer to the return address */
+static context* runningThread = NULL; /* global for the currently running thread */
+static context* head = NULL;
 static thread shead = NULL;
 static tid_t tidCount = 1;
-static void* returnSP; /* pointer to the return address */
 
 
 /* The job of lwp create() is to set up a thread’s context
@@ -37,25 +41,25 @@ tid_t lwp_create(lwpfun function, void *argument, size_t stacksize) {
     * else set up doubly linked list of threads.
     */
    if (tidCount == 1) {
-      iter->prev = NULL; 
-      iter->next = NULL;
+      iter->tprev = NULL; 
+      iter->tnext = NULL;
 	   head = iter; 
    }
    else {
-      for ( ; iter->next; iter = iter->next)
+      for ( ; iter->tnext; iter = iter->tnext)
          ;
-      iter->next = malloc(sizeof (context));
+      iter->tnext = malloc(sizeof (context));
       
       /* return error value if malloc fails */
-      if (iter->next == NULL)
+      if (iter->tnext == NULL)
          return (tid_t) -1;
       
       /* set prev to prior last item in list, 
        * go to new last item and set next to NULL
        */
-      iter->next->prev = iter;
-      iter = iter->next;
-      iter->next = NULL;
+      iter->tnext->tprev = iter;
+      iter = iter->tnext;
+      iter->tnext = NULL;
    }
    
    /* assign tid, stack, and stack size to thread */
@@ -64,7 +68,7 @@ tid_t lwp_create(lwpfun function, void *argument, size_t stacksize) {
    iter->stacksize = stacksize;
   
    /* return error value if stack malloc fails */
-   if (iter->next == NULL)
+   if (iter->tnext == NULL)
       return (tid_t)-1;
    
    /* assign function parameters to stack, including return address */
@@ -75,16 +79,16 @@ tid_t lwp_create(lwpfun function, void *argument, size_t stacksize) {
    tempBP = --tempSP;
    *(--tempSP) = (unsigned long) tempBP;
    
-   iter->stack->rdi = (unsigned long) argument;
-   iter->state->rsp = tempSP;
-   iter->state->rbp = tempBP;
+   iter->state.rdi = (unsigned long) argument;
+   iter->state.rsp = (unsigned long) tempSP;
+   iter->state.rbp = (unsigned long) tempBP;
    
    return iter->tid;
 }
 
 /* terminates the calling LWP */
 void lwp_exit(void) {
-	context ctx = sched.next();
+	context *ctx = sched->next();
 	
 	if (!ctx) {
 		lwp_stop();
@@ -96,10 +100,10 @@ void lwp_exit(void) {
  *
  * */
 tid_t lwp_gettid(void) {
-   thread temp = sched.next();
+   thread temp = sched->next();
    int i = 1;	
 
-   for (; temp && temp != runningThread; i++, temp = temp->next)
+   for (; temp && temp != runningThread; i++, temp = temp->tnext)
       ;
 
    if (temp != NULL)
@@ -115,15 +119,15 @@ tid_t lwp_gettid(void) {
  * the stack
  */
 void lwp_yield(void) {
-	thread *t = sched.next();
+	thread t = sched->next();
 	rfile currentRegisters;
 
-	save_context(runningThread->state); 
+	save_context(&(runningThread->state)); 
 
 	if (t != NULL) {
 		runningThread = t;
 		SetSP(t->stack);	
-		load_context(t->state);
+		load_context(&(t->state));
 	}
 	else 	
 		lwp_stop();
@@ -140,17 +144,17 @@ void lwp_start(void) {
    GetSP(returnSP);
    
    /* save the base pointer, instruction pointer, etc */
-   save_context(returnContext);
+   save_context(&(returnContext));
    
    /* picks the next thread in the schedule, loads it's context */
-   runningThread = sched.next();
+   runningThread = sched->next();
    
    if (runningThread == NULL) {
-      load_context(returnContext);
+      load_context(&(returnContext));
       return;
    }
    
-   load_context(runningThread->context);
+   load_context(&(runningThread->state));
 }
 
 /* stop the LWP system. 
@@ -159,22 +163,22 @@ void lwp_start(void) {
 void lwp_stop(void) {
    SetSP(returnSP);
 
-   load_context(returnContext); /* load the registers from the return context global */
+   load_context(&(returnContext)); /* load the registers from the return context global */
 }
 
 /* install a new scheduling function */
 void lwp_set_scheduler(scheduler fun) {
-   thread t = head;
+   thread t = shead;
 
-   for ( ; t; t = t->next()) {
-      sched.remove(t);
-      fun.admit(t);
+   for ( ; t; t = t->snext) {
+      sched->remove(t);
+      fun->admit(t);
    }
 
-   if (sched.shutdown != NULL)
-      sched.shutdown();
+   if (sched->shutdown != NULL)
+      sched->shutdown();
 
-   sched = *fun;
+   sched = fun;
 }
 
 /* find out what the current scheduler is */
@@ -184,17 +188,14 @@ scheduler lwp_get_scheduler(void) {
 
 /* map a thread id to a context */
 thread tid2thread(tid_t tid) {
-   context iter;
-   context ctx = sched->next()
+   thread iter;
 
-   if (ctx) {
-      for (iter = head; iter && iter->tid != tid; iter = iter->next)
-         ;
-      if (iter->tid == tid)
-         return iter;
-   }
+   for (iter = head; iter && iter->tid != tid; iter = iter->tnext)
+       ;
+   if (iter->tid == tid)
+       return iter;
 
-   return NULL; 
+   return NO_THREAD; 
 }
 
 /* 
@@ -226,11 +227,20 @@ void rr_shutdown() {
 }
 
 void rr_admit(thread new) {
- 
-	for (iter = head; iter->next; iter = iter->next)
+	thread iter;
+	
+	if (shead == NULL) {
+		shead = new;
+		new->snext = NULL;
+		new->sprev = NULL;
+		return;
+	}
+ 	
+	/* go to the last item of the list */
+	for (iter = head; iter->snext; iter = iter->snext)
          ;
       
-      
+	      
 /* set prev to prior last item in list, 
 * go to new last item and set next to NULL
 */
@@ -244,22 +254,29 @@ void rr_admit(thread new) {
 		new->sprev = iter;
 	}
 
-
 	return;
 }
 
 void rr_remove(thread victim) {
-	/* sever the connection to iter->snext */
-	
+
+	/* special case if we're at the head */	
 	if (victim == shead) {
 		if (victim->snext) {
-			victim->next->sprev = NULL;
+			victim->snext->sprev = NULL;
 			shead = victim->snext;
 		}
+	
 		else {
 			shead = NULL;
 		}
 	
+	}
+	
+	if (victim->sprev) {
+		victim->sprev->snext = victim->snext;
+	}
+	if (victim->snext) {
+		victim->snext->sprev = victim->sprev;
 
 	}
 	victim->snext = NULL;
@@ -274,8 +291,8 @@ void rr_remove(thread victim) {
  * running process's next. if the currently running process's
  * next is null, start at the head of the list.
  * */
-context rr_next() {
-   context iter = runningThread->next?runningThread->next:head;
+thread rr_next() {
+   thread iter = runningThread->snext?runningThread->snext:shead;
 
 	/* this is basically a check to see if there is only 1 entry 
  	* in the linked list*/
@@ -283,16 +300,7 @@ context rr_next() {
 		return runningThread;
 	}	
 
-	for (; iter != runningThread; iter = iter->snext?iter->snext:shead)
-		;
-   /* this is basically a check to see if there is only 1 entry 
-    * in the linked list
-    */
-   if (runningThread == head && runningThread->next == NULL) {
-      return runningThread;
-   }	
-
-   for (; iter != runningThread; iter = iter->next?iter->next:head)
+   for (; iter != runningThread; iter = iter->snext?iter->snext:head)
       ;
 
    return iter;
